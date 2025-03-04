e63fb46f76a7b419281d601025670cd562de64ae
Zikrullo Amiri <48012861+amirymax@users.noreply.github.com>
2025-03-04 14:31:28 +0300

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -34,7 +34,7 @@ const Contact = () => {
     const message = `
       📝 *Новое сообщение с сайта:*
       👤 *Имя*: ${formData.name}
-      🔗 *Telegram*: ${formData.telegram}
+      🔗 *TG*: ${formData.telegram}
       📧 *Email*: ${formData.email}
       💬 *Сообщение*: ${formData.message}
     `;
